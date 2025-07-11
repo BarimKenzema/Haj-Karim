e95816f564854c99675ee499184345f2c74862b0
Shamshama <Berimbinimchemishepanahbarxudesh@proton.me>
2025-07-11 04:40:51 +0330

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ except ImportError as e:
 API_ID = os.environ.get('TELEGRAM_API_ID')
 API_HASH = os.environ.get('TELEGRAM_API_HASH')
 SESSION_STRING = os.environ.get('TELETHON_SESSION')
-CONFIG_CHUNK_SIZE = 111
+CONFIG_CHUNK_SIZE = 444
 
 # --- Helper Functions ---
 def setup_directories():
